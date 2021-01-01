ce88d3c93a42cce6bc8fedc8b5347c7d595e036b
Jen <jenhalv06@gmail.com>
2021-01-01 11:46:26 -0600

    recipe color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from '../../../../../GitHub/Portfolio-JenHalvorson/src/app/about/about.component';
+import { TraditionComponent } from './info/tradition/tradition.component';
+import { ProductComponent } from './info/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'product',
+    component: ProductComponent
+  },
+  {
+    path: 'traditon',
+    component: TraditionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
